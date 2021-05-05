822555ae4a6d2aa735ea0716f341b374f3a34465
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-05-05 02:18:21 +0200

    Fixed tests

--- angular-start/e2e/src/app.e2e-spec.ts
+++ angular-start/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('angular-start app is running!');
+    expect(await page.getTitleText()).toEqual('Hello World app is running!');
   });
 
   afterEach(async () => {
