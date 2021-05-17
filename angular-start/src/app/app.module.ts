f525de61b64279559dab1e80c9e2b001f13686b9
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-05-18 00:02:02 +0200

    Added component

--- angular-start/src/app/app.module.ts
+++ angular-start/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesDisplayerComponent } from './quotes-displayer/quotes-displayer.component';
 import { ClickCounterComponent } from './click-counter/click-counter.component';
+import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesDisplayerComponent,
-    ClickCounterComponent
+    ClickCounterComponent,
+    ProfileEditorComponent
   ],
   imports: [
     BrowserModule,
