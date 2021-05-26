bb10ef303f69560dd8979e66a6d91a492a576baf
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-05-27 01:15:09 +0200

    Added implementation

--- angular-start/src/app/random-item-with-id.service.spec.ts
+++ angular-start/src/app/random-item-with-id.service.spec.ts
@@ -24,4 +24,15 @@ describe('RandomItemWithIdService', () => {
     expect(results[1].name).toEqual("a1");
     expect(results[24].name).toEqual("a24");
   });
+
+  it('should return correct results for second chunk', () => {
+    // WHEN
+    const results = service.findItems('bc', 1, 25, 250);
+
+    // THEN
+    expect(results.length).toEqual(25);
+    expect(results[0].name).toEqual("bc25");
+    expect(results[1].name).toEqual("bc26");
+    expect(results[24].name).toEqual("bc49");
+  });
 });
---
+++ angular-start/src/app/random-item-with-id.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { ItemWithId } from "./item-with-id";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RandomItemWithIdService {
+
+  constructor() { }
+
+  public findItems(prefix:string, start:number = 0, limit:number = 25, max:number = 250):ItemWithId[]
+  {
+    if (limit > 0)
+    { 
+      let results:ItemWithId[] = new Array();
+      if (start == 0) 
+      {
+        results.push(new ItemWithId(prefix, 0));
+        for (let i = 1; i < limit && i < max; i++) 
+        {
+          results.push(new ItemWithId(prefix + i, i));
+        }
+      } else {
+        let startIndex = start * limit;
+        for (let i = startIndex; i < startIndex + limit && i < max; i++) 
+        {
+          results.push(new ItemWithId(prefix + i, i));
+        }
+      }
+      return results;
+    }
+    return new Array();
+  }
+}
