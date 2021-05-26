c37d7e06d78c9d4cf6a831f6b690fbc3ccc8556c
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-05-27 01:07:56 +0200

    Added service - not yet finished!

---
+++ angular-start/src/app/random-item-with-id.service.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed } from '@angular/core/testing';
+
+import { RandomItemWithIdService } from './random-item-with-id.service';
+
+describe('RandomItemWithIdService', () => {
+  let service: RandomItemWithIdService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(RandomItemWithIdService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return correct results for first chunk', () => {
+    // WHEN
+    const results = service.findItems('a', 0, 25, 250);
+
+    // THEN
+    expect(results.length).toEqual(25);
+    expect(results[0].name).toEqual("a");
+    expect(results[1].name).toEqual("a1");
+    expect(results[24].name).toEqual("a24");
+  });
+});
