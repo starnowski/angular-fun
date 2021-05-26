8d3efcbeafbfba7307c498cdf66c1ceef780d6dd
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-05-27 01:22:37 +0200

    Added tests cases

--- angular-start/src/app/random-item-with-id.service.spec.ts
+++ angular-start/src/app/random-item-with-id.service.spec.ts
@@ -35,4 +35,23 @@ describe('RandomItemWithIdService', () => {
     expect(results[1].name).toEqual("bc26");
     expect(results[24].name).toEqual("bc49");
   });
+
+  it('should return only max number of recors for first chunk', () => {
+    // WHEN
+    const results = service.findItems('c', 0, 21, 13);
+
+    // THEN
+    expect(results.length).toEqual(13);
+    expect(results[0].name).toEqual("c");
+    expect(results[1].name).toEqual("c1");
+    expect(results[12].name).toEqual("c12");
+  });
+
+  it('should return empty array when index is larger than max number of results', () => {
+    // WHEN
+    const results = service.findItems('d', 11, 30, 300);
+
+    // THEN
+    expect(results.length).toEqual(0);
+  });
 });
