828db85b188d75d73946467da9a8d7ee499f8407
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-05-27 23:51:24 +0200

    Added component

--- angular-start/src/app/app-routing.module.ts
+++ angular-start/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { FirstRouteComponent } from './first-route/first-route.component';
 import { SecondRouteComponent } from './second-route/second-route.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NgSelectExampleComponent } from './ng-select-example/ng-select-example.component';
+import { SelectScrollExampleComponent } from './select-scroll-example/select-scroll-example.component';
 
 const routes: Routes = [
   { path: 'first-component', component: FirstRouteComponent },
   { path: 'second-component', component: SecondRouteComponent },
   { path: 'ng-select-example-component', component: NgSelectExampleComponent },
+  { path: 'select-scroll-example', component: SelectScrollExampleComponent },
   { path: '',   redirectTo: '/first-component', pathMatch: 'full' }, // redirect to `first-component`
   { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ];
