aa9d4aca0725a11c97d442df934b8b1d2265ace6
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-05-29 14:21:20 +0200

    Added tests case (not yet finished)

--- angular-start/src/app/select-scroll-example/select-scroll-example.component.spec.ts
+++ angular-start/src/app/select-scroll-example/select-scroll-example.component.spec.ts
@@ -1,14 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RandomItemWithIdService } from "../random-item-with-id.service";
 import { SelectScrollExampleComponent } from './select-scroll-example.component';
 
 describe('SelectScrollExampleComponent', () => {
   let component: SelectScrollExampleComponent;
   let fixture: ComponentFixture<SelectScrollExampleComponent>;
+  let findItemsSpy: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SelectScrollExampleComponent ]
+      declarations: [ SelectScrollExampleComponent ],
+      providers: [
+        RandomItemWithIdService
+      ]
     })
     .compileComponents();
   });
@@ -16,10 +20,28 @@ describe('SelectScrollExampleComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SelectScrollExampleComponent);
     component = fixture.componentInstance;
+    const randomItemWithIdService = TestBed.inject(RandomItemWithIdService);
+    findItemsSpy = spyOn(randomItemWithIdService, 'findItems');
+    findItemsSpy.and.returnValue([]);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should invoke service on input click', () => {
+    // GIVEN
+    let input = fixture.debugElement.nativeElement.querySelector('.ng-select input.ng-input');
+
+    // WHEN
+    input.triggerEventHandler('focus', null);
+
+    // THEN
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(findItemsSpy.calls.count()).toEqual(1);
+    });
+  });
+
 });
