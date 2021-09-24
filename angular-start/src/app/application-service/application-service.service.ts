52ee635c043d28ca1584cdb0d3a55b6f398337f1
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-09-24 02:18:28 +0200

    Code fix.

--- angular-start/src/app/application-service/application-service.service.ts
+++ angular-start/src/app/application-service/application-service.service.ts
@@ -14,11 +14,26 @@ export class ApplicationServiceService {
   constructor(private http: HttpClient) { }
 
   getApplicationConfig(): Observable<ApplicationConfig> {
-  {
     const url = `/application-config`;
     return this.http.get<ApplicationConfig>(url)
       .pipe(
         catchError(this.handleError<ApplicationConfig>(`get application config`))
       );
   }
+
+  private handleError<T>(operation = 'operation') {
+      return (error: HttpErrorResponse): Observable<T> => {
+
+        // TODO: send the error to remote logging infrastructure
+        console.error(error); // log to console instead
+
+        const message = (error.error instanceof ErrorEvent) ?
+          error.error.message :
+         `server returned code ${error.status} with body "${error.error}"`;
+
+        // TODO: better job of transforming error for user consumption
+        throw new Error(`${operation} failed: ${message}`);
+      };
+
+    }
 }
