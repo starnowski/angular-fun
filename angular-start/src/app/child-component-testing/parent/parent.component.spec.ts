319c7788714f7216504933809283a131e2995e0e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-10-14 02:30:07 +0200

    Added tests case

--- angular-start/src/app/child-component-testing/parent/parent.component.spec.ts
+++ angular-start/src/app/child-component-testing/parent/parent.component.spec.ts
@@ -3,9 +3,11 @@ import { ChildComponent } from '../child/child.component';
 
 import { ParentComponent } from './parent.component';
 import { MockComponent } from 'ng-mocks';
+import { By } from '@angular/platform-browser';
 
 describe('ParentComponent', () => {
   let component: ParentComponent;
+  let childComponent: ChildComponent;
   let fixture: ComponentFixture<ParentComponent>;
 
   beforeEach(async () => {
@@ -18,10 +20,16 @@ describe('ParentComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ParentComponent);
     component = fixture.componentInstance;
+    childComponent = fixture.debugElement.query(By.directive(ChildComponent)).componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should init properties', () => {
+    expect(childComponent.property1).toBeTruthy();
+    expect(childComponent.property2).toEqual("this is raw string");
   });
 });
