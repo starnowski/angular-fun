5acf4982f676d5f4b480079fe0b93b8cb2df3b84
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-10-15 09:53:17 +0200

    Added test for parsing xml

--- angular-start/src/app/codemirror-test1/xmlutil.service.spec.ts
+++ angular-start/src/app/codemirror-test1/xmlutil.service.spec.ts
@@ -13,4 +13,62 @@ describe('XmlutilService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  const expectedCorrectXmls: string[] = [
+    `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <note>
+    <to>Tove</to>
+    <from>Jani</from>
+    <heading>Reminder</heading>
+    <body>Don't forget me this weekend!</body>
+    </note>
+    `,
+    `
+    <top>
+    <child name="Simon"/>
+    </top>
+    `
+  ];
+  expectedCorrectXmls.forEach(function (expectedCorrectXml) {
+  it('should confirm that xml is correct: ' + expectedCorrectXml, (done: DoneFn) => {
+    // WHEN
+    const result = service.validate(expectedCorrectXml);
+
+    // THEN
+    expect(result).toBeTrue();
+  })
+});
+
+  const expectedInvalidXmls: string[] = [
+    `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <note>
+    <to>
+    </note>
+    `,
+    `
+    <top>
+    <child name="Simon"/>
+    </top>
+    <top1>
+    <child name="Simon"/>
+    </top1>
+    `,
+    `
+    <top>
+    <child name="Simon">
+    </top1>
+    `
+  ];
+  expectedInvalidXmls.forEach(function (expectedInvalidXml) {
+  it('should confirm that xml is incorrect: ' + expectedInvalidXml, (done: DoneFn) => {
+    // WHEN
+    const result = service.validate(expectedInvalidXml);
+
+    // THEN
+    expect(result).toBeFalse();
+  })
+});
+
 });
