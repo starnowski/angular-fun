bb62cfca61d23e2c7254178576ead7ef694a114f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-10-15 02:14:33 +0200

    Added tests for codemirror

--- angular-start/src/app/codemirror-test1/codemirror-binding/codemirror-binding.component.spec.ts
+++ angular-start/src/app/codemirror-test1/codemirror-binding/codemirror-binding.component.spec.ts
@@ -6,6 +6,7 @@ import { CodemirrorBindingComponent } from './codemirror-binding.component';
 describe('CodemirrorBindingComponent', () => {
   let component: CodemirrorBindingComponent;
   let fixture: ComponentFixture<CodemirrorBindingComponent>;
+  let xmlutilServiceSpy: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,10 +19,42 @@ describe('CodemirrorBindingComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CodemirrorBindingComponent);
     component = fixture.componentInstance;
+    const xmlutilService = TestBed.inject(XmlutilService);
+    xmlutilServiceSpy = spyOn(xmlutilService, 'validate');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set xml as valid when xmlservice confirms it', () => {
+    // GIVEN
+    xmlutilServiceSpy.and.returnValue(true);
+
+    // WHEN
+    component.content = "<x></x>";
+
+    // THEN
+    fixture.detectChanges();
+    expect(component.valid).toBeTrue();
+    fixture.whenStable().then(() => {
+      expect(xmlutilServiceSpy.calls.count()).toEqual(1);
+    });
+  });
+
+  it('should set xml as invalid when xmlservice confirms it', () => {
+    // GIVEN
+    xmlutilServiceSpy.and.returnValue(false);
+
+    // WHEN
+    component.content = "<x></x>";
+
+    // THEN
+    fixture.detectChanges();
+    expect(component.valid).toBeFalse();
+    fixture.whenStable().then(() => {
+      expect(xmlutilServiceSpy.calls.count()).toEqual(1);
+    });
   });
 });
