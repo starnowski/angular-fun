006766a602a82f72366f11bf76be1ee833724e14
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-12-13 21:58:37 +0100

    Added next test

--- angular-start/src/app/operators/contact-map.spec.ts
+++ angular-start/src/app/operators/contact-map.spec.ts
@@ -7,13 +7,35 @@ describe("contactMap operations", () => {
     beforeAll(async () => {
     });
 
-    it("should count all documents", async () => {
+    it("should execute concatMap", async () => {
         // GIVEN
         let obs= of(1,2,3,4)
         let results = new Array();
         let expectedResults = [2, 4, 6, 8];
  
         let stream = obs.pipe(
+        concatMap( val => {
+            return of(val*2)  //Returning observable
+        })
+        )
+        .subscribe(ret=> {
+            results.push(ret);
+        });
+
+        // THEN
+        expect(results).toEqual(expectedResults);
+      });
+
+      it("should execute two concatMap", async () => {
+        // GIVEN
+        let obs= of(1,2,3,4)
+        let results = new Array();
+        let expectedResults = [4, 8, 12, 16];
+ 
+        let stream = obs.pipe(
+        concatMap( val => {
+            return of(val*2)  //Returning observable
+        }),
         concatMap( val => {
             return of(val*2)  //Returning observable
         })
